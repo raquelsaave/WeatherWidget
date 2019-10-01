647f8f9d433071aec1b6b91ee3089e1803a33ab7
Cristian Colorado <ccoloradoc@gmail.com>
2019-10-01 16:51:27 -0500

    Places API (#3)

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
-var connect = require("connect");
-var serveStatic = require("serve-static");
-connect().use(serveStatic(__dirname)).listen(8080, function () {
-    console.log("Server running on 8080...");
+
+var fs = require('fs');
+var path = require('path');
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+/* LOADING PLACES FOR API */
+let places = [];
+fs.readFile(path.join(__dirname, '/assets/city.list.json'), 'utf8', function(err, contents) {
+	places = JSON.parse(contents);
 });
+
+// API ROUTES
+
+var router = express.Router();
+
+router.get('/search', function(req, res) {
+	let term = req.query.place.toLowerCase();
+	let limit = req.query.limit || 10;
+	let result = places.filter(place => place.name.toLowerCase().indexOf(term) >= 0);
+	console.log(`>> Looking for ${term} found ${result.length} items. Retrieving ${limit}`);
+	res.json(result.slice(0, limit));   	
+});
+
+app.use('/api', router);
+
+app.listen(port);
+
+console.log('Magic happens on port ' + port);
