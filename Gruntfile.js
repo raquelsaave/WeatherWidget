aa81ec580e53c8b5b4a5219a6d211ddbb02c59ab
raquelsaave <49658144+raquelsaave@users.noreply.github.com>
2019-10-18 15:10:54 -0500

    Project structure (#7)
    * Cambiando estrictura del proyecto
    
    * Cambiando scss
    
    * Webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,42 @@
-const sass = require('node-sass');
+const sass = require("node-sass");
 /*global module:false*/
 module.exports = function (grunt) {
-  // Project configuration.
-  grunt.initConfig({
-    // Task configuration.
-    watch: {
-    },
-    stylelint: {
-      all: ["assets/css/main.css"]
-    },
-    sass: {
-      options: {
-        implementation: sass,
-        sourceMap: true
-      },
-      dist: {
-        src: ['main.scss'],
-        dest: 'assets/css',
-        ext: '.css',
-        cwd: 'assets/scss',
-        expand: true
-      },
-    },
-    eslint: {
-      target: ["assets/js/*.js"]
-    }
+	// Project configuration.
+	grunt.initConfig({
+		// Task configuration.
+		watch: {
+		},
+		stylelint: {
+			all: ["src/scss/*.scss", "src/scss/ui/*.scss"]
+		},
+		sass: {
+			options: {
+				implementation: sass,
+				sourceMap: true
+			},
+			dist: {
+				src: ["main.scss"],
+				dest: "assets/css",
+				ext: ".css",
+				cwd: "src/scss",
+				expand: true
+			},
+		},
+		eslint: {
+			target: ["src/js/components/*.js", "src/js/pages/*.js", "src/js/services/*.js"]
+		},
+		exec: {
+			webpack: {command: "npm run webpack"},
+			webpackdev: {command: "npm run webpackdev"}
+		}
+	});
 
-  });
+	grunt.loadNpmTasks("grunt-stylelint");
+	grunt.loadNpmTasks("grunt-sass");
+	grunt.loadNpmTasks("grunt-eslint");
+	grunt.loadNpmTasks("grunt-exec");
 
-  grunt.loadNpmTasks("grunt-stylelint");
-  grunt.loadNpmTasks("grunt-sass");
-  grunt.loadNpmTasks('grunt-eslint');
-
-  // Default task.
-  grunt.registerTask('default', ['stylelint', 'sass']);
+	// Default task.
+	grunt.registerTask("default", ["stylelint", "sass", "eslint", "exec:webpackdev"]);
+	grunt.registerTask("build", ["stylelint", "sass", "eslint", "exec:webpack"]);
 };
--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,3 +1,7 @@
+import PlaceService from "../services/place-service.js"
+import DashboardComponent from "../components/dashboard-component.js"
+import SearchComponent from "../components/search-component.js"
+
 const root = document.querySelector("#form")
 let dashboardRoot = document.querySelector(".page-content");
 
